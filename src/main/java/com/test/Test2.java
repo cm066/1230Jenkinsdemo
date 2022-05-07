38759a05f11344f777539037c9f8a3f476f0120b
lufei <59244907+cm066@users.noreply.github.com>
2022-05-07 23:23:39 +0800

    Update Test2.java
    add print

--- src/main/java/com/test/Test2.java
+++ src/main/java/com/test/Test2.java
@@ -8,5 +8,6 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("test git command");
         System.out.println(66666);
+        System.out.println(7777);
     }
 }
