b3a96af0020f5f9276504954cff535cbed3710d1
cmcm <1446525549@qq.com>
2022-05-07 22:59:06 +0800

    first commit1

---
+++ src/main/java/com/test/Test1.java
@@ -0,0 +1,11 @@
+package com.test;
+
+/**
+ * @author cm
+ * @create 2022/5/7-10:44 下午
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/main/java/com/test/Test2.java
@@ -0,0 +1,11 @@
+package com.test;
+
+/**
+ * @author cm
+ * @create 2022/5/7-10:53 下午
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("test git command");
+    }
+}
